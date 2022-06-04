3c70314bb147f7dedec898579c9bbd87db394482
Rishabh Vasudevan <rishabhvasudevan19@gmail.com>
2022-06-04 22:18:14 +0530

    first commit

---
+++ main/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class NFTData(models.Model):
+    key = models.CharField(max_length=200)
+    image_url = models.URLField(max_length=200)
---
+++ main/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render,HttpResponse
+from main.models import NFTData
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.core import serializers
+from django.http import JsonResponse
+
+
+# Create your views here.
+
+
+@csrf_exempt
+def nft(request):
+    if request.method == 'GET':
+        NFT_json = serializers.serialize("json", NFTData.objects.all())
+        return HttpResponse(NFT_json, content_type='application/json')
+    else:
+        data = request.body.decode('utf-8')
+        recieved_data = json.loads(data)
+        obj = NFTData(key =  recieved_data['name'], image_url = recieved_data['image_url'])
+        obj.save()
+        return HttpResponse("successful")
