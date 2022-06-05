c88e6b95b676850d0ce2fbcbadc6e1a503c77bd6
Rishabh Vasudevan <rishabhvasudevan19@gmail.com>
2022-06-06 00:39:15 +0530

    deploying on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT nft_backend.wsgi:application
--- nft_backend/settings.py
+++ nft_backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-un-&g&8j5nu+37-g%9$4_4kitrp^ti)de&m86goa0f&dyh1--z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
