abf833bce0ae500356fda2d9f2a2c001a6bd1021
Rishabh Vasudevan <rishabhvasudevan19@gmail.com>
2022-06-06 17:40:01 +0530

    making all types of json acceptable

--- main/models.py
+++ main/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 
 class NFTData(models.Model):
-    key = models.CharField(max_length=200)
-    image_url = models.URLField(max_length=200)
+    # key = models.CharField(max_length=200)
+    # image_url = models.URLField(max_length=200)
+    data = JSONField(null = True, blank = True)
